bd8e0c3127edb5595fbb6d39080fb7debc978820
Jon Targ <jon@karmacreative.io>
2018-08-23 14:41:00 -0500

    Remove unused steps from install generator

--- lib/generators/solidus_seo/install/install_generator.rb
+++ lib/generators/solidus_seo/install/install_generator.rb
@@ -7,30 +7,6 @@ module SolidusSeo
 
       source_root File.expand_path("../templates", __FILE__)
 
-      def add_javascripts
-        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/solidus_seo\n"
-        append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/solidus_seo\n"
-      end
-
-      def add_stylesheets
-        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/solidus_seo\n", before: /\*\//, verbose: true
-        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/solidus_seo\n", before: /\*\//, verbose: true
-      end
-
-      def add_migrations
-        run 'bundle exec rake railties:install:migrations FROM=solidus_seo'
-      end
-
-      def run_migrations
-        run_migrations = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask('Would you like to run the migrations now? [Y/n]'))
-
-        if run_migrations
-          run 'bundle exec rake db:migrate'
-        else
-          puts 'Skipping rake db:migrate, don\'t forget to run it!'
-        end
-      end
-
       def install_image_optim
         run 'rails generate paperclip_optimizer:install'
         copy_file 'image_optim.yml', 'config/image_optim.yml'
