e23ec5acaba169148bb0361645e90d1729a7c0cf
Jon Targ <jon@karmacreative.io>
2018-08-28 19:34:03 -0500

    Add helpers to Spree::StoreController to fix helpers 'disappearing' when added to ApplicationController

--- app/controllers/application_controller_decorator.rb
+++
@@ -1,3 +0,0 @@
-ApplicationController.class_eval do
-  helper SolidusSeo::Jsonld::TagHelper
-end
--- app/controllers/spree/store_controller_decorator.rb
+++ app/controllers/spree/store_controller_decorator.rb
@@ -1,3 +1,4 @@
 Spree::StoreController.class_eval do
   include SolidusSeo::MetaDataBuilder
+  helper SolidusSeo::Jsonld::TagHelper
 end
