2b5148438da97ef9e45d46ae0c8eb7615188e693
Jon Targ <jon@karmacreative.io>
2018-09-07 03:35:21 -0500

    Add custom testing helpers; extend spree factories

--- lib/solidus_seo/factories.rb
+++ lib/solidus_seo/factories.rb
@@ -1,8 +1,19 @@
 # frozen_string_literal: true
-
+# require 'spree/testing_support/factories/store_factory.rb'
 FactoryBot.define do
   # Define your Spree extensions Factories within this file to enable applications, and other extensions to use and override them.
   #
   # Example adding this to your spec_helper will load these Factories for use:
   # require 'solidus_seo/factories'
+end
+
+FactoryBot.modify do
+  factory :store, class: Spree::Store do
+    seo_title { 'My Store SEO name' }
+    meta_description { 'My Store SEO description' }
+  end
+
+  factory :base_product, class: Spree::Product do
+    meta_description { 'My Product SEO description' }
+  end
 end
---
+++ spec/support/testing_helpers.rb
@@ -0,0 +1,12 @@
+module TestingHelpers
+  def add_stubs(target, atts = {})
+    target = create(target.to_sym) if target.is_a? Symbol
+
+    target.tap do |it|
+      atts.each do |(attr, v)|
+        # puts "-----> #{it} : #{attr}=#{v}"
+        allow_any_instance_of(it.class).to receive(attr.to_sym) { v }
+      end
+    end
+  end
+end
