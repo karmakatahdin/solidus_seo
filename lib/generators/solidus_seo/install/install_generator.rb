a06503276255b7177474aed0c9f5d2c8a4d70c45
Thomas Sample <thomas@mfrwdesign.com>
2018-10-07 17:32:04 -0400

    Move overrides to install generator

--- lib/generators/solidus_seo/install/install_generator.rb
+++ lib/generators/solidus_seo/install/install_generator.rb
@@ -12,6 +12,23 @@ module SolidusSeo
         copy_file 'image_optim.yml', 'config/image_optim.yml'
         copy_file 'paperclip_optimizer.rb', 'config/initializers/paperclip_optimizer.rb', force: true
       end
+
+      def install_overrides
+        if ENV["RAILS_ENV"] == "test"
+          copy_overrides
+        else
+          overrides_choice = ask("Do you want to install the solidus_frontend overrides into your app? (y/n)", :limited_to => [“y”, “n”])
+          if (overrides_choice == "y") then copy_overrides
+        end
+      end
+
+      def copy_overrides
+        copy_file 'insert_dump_jsonld_helper.html.erb.deface', 'app/overrides/spree/layouts/spree_application/insert_dump_jsonld_helper.html.erb.deface'
+        copy_file 'insert_product_jsonld_helper.html.erb.deface', 'app/overrides/spree/products/show/insert_product_jsonld_helper.html.erb.deface'
+        copy_file 'insert_display_meta_tags_helper.html.erb.deface', 'app/overrides/spree/shared/_head/insert_display_meta_tags_helper.html.erb.deface'
+        copy_file 'remove_original_title_tag.deface', 'app/overrides/spree/shared/_head/remove_original_title_tag.deface'
+        copy_file 'insert_product_list_helper.html.erb.deface', 'app/overrides/spree/shared/_products/insert_product_list_helper.html.erb.deface'
+      end
     end
   end
 end
