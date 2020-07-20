5b799066403c6be82b3b6edb7d44585ed561124a
Jon Targ <jon@karmacreative.io>
2020-07-20 08:55:32 -0500

    Load gem engine at last

--- lib/solidus_seo.rb
+++ lib/solidus_seo.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
+
 require 'solidus_core'
-require 'solidus_seo/engine'
 require 'solidus_support'
 require 'meta_tags'
 require 'paperclip-optimizer'
+require 'solidus_seo/engine'
